c00491df12f31fdcf6b0062c46422a38dc0fcecc
Tuure Laurinolli <tuure@laurinolli.net>
2012-04-11 01:40:54 +0300

    - Different line for testing - Manual encoding of string so that pipes work OK

--- print_kalkati_pickle.py
+++ print_kalkati_pickle.py
@@ -4,5 +4,5 @@ import cPickle
 services = cPickle.load(sys.stdin)
 print len(services)
 for service in services:
-    if service.line_id.startswith('501'):
-        print unicode(service)
+    if service.line_id.startswith('63'):
+        print unicode(service).encode('utf-8')
